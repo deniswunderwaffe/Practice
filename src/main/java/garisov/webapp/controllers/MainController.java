0c2d99a2fc7d5c66e113419491dfd14c6a12b901
Denis <dgarisov@gmail.com>
2021-03-31 15:58:53 +0300

    Never start from end. Forgot to connect drivers and to configurate DispatcherServlet. Show all/Show id release

--- src/main/java/garisov/webapp/controllers/MainController.java
+++ src/main/java/garisov/webapp/controllers/MainController.java
@@ -1,6 +1,7 @@
 package garisov.webapp.controllers;
 
 import garisov.webapp.dao.MedicDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,17 +11,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/main")
 public class MainController {
-    final MedicDAO medicDao;
+
+    private final MedicDAO medicDao;
+    @Autowired
     public MainController(MedicDAO medicDao) {
         this.medicDao = medicDao;
     }
 
     @GetMapping()
-    public String main(){
+    public String main(Model model)
+    {
+        model.addAttribute("medics",medicDao.show());
         return "main";
     }
     @GetMapping("/{id}")
     public String  show(@PathVariable("id")int id, Model model){
-        return "";
+        model.addAttribute("medic",medicDao.showId(id));
+        return "medic/show";
     }
 }
